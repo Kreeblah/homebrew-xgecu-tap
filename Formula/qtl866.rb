a225ecbb3952f9eb37ce4fcc186f2a4ce68e9561
Kreeblah <kreeblah@gmail.com>
2020-04-07 22:02:17 -0700

    Added dependencies and additional bin install

--- Formula/qtl866.rb
+++ Formula/qtl866.rb
@@ -3,11 +3,13 @@ class Qtl866 < Formula
   homepage "https://github.com/wd5gnr/qtl866"
   head "https://github.com/wd5gnr/qtl866.git", :revision => "1173c3e048c9b777611b379892f397dd5c64a261"
 
+  depends_on "minipro"
   depends_on "qt"
 
   def install
     system "qmake"
     system "make"
+    bin.install "binhexedit"
     prefix.install "qtl866.app"
     prefix.install_symlink "/Applications/qtl866.app" => prefix/"qtl866.app"
   end
