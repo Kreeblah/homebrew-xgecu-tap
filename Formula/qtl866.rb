91aa27b62c2361ac487f30f7f5ddf14fd711f392
Kreeblah <kreeblah@gmail.com>
2020-04-07 19:05:59 -0700

    Added formula for qtl866

---
+++ Formula/qtl866.rb
@@ -0,0 +1,18 @@
+class Qtl866 < Formula
+  desc "GUI for minipro tool"
+  homepage "https://github.com/wd5gnr/qtl866"
+  head "https://github.com/wd5gnr/qtl866.git", :revision => "1173c3e048c9b777611b379892f397dd5c64a261"
+
+  depends_on "qt"
+
+  def install
+    system "qmake"
+    system "make"
+    prefix.install "qtl866.app"
+    prefix.install_symlink "/Applications/qtl866.app" => prefix/"qtl866.app"
+  end
+
+  test do
+    True
+  end
+end
